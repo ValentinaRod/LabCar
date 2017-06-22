dc38765f2768042e8f39997c66277dc2e12f3b2d
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-22 02:55:49 -0400

    añadir

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
 /*hamburguesa*/
-$(document).ready(function() {   
-            var sideslider = $('[data-toggle=collapse-side]');
-            var sel = sideslider.attr('data-target');
-            var sel2 = sideslider.attr('data-target-2');
-            sideslider.click(function(event){
-                $(sel).toggleClass('in');
-                $(sel2).toggleClass('out');
-            });
-        });
-/*----------------------------------------------*/
+document.getElementById("navbar-toggle").addEventListener(click, function(){
+	document.getElementById("menu-container").classList.toggle.("menu-ver");
+});
+/*--------------------------------*/
