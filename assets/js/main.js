0efa201f6c1c63da81094d9ca0ad532d7fd5d9ad
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-22 09:35:48 -0400

    añadida seccion 3

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-/*hamburguesa*/
-document.getElementById("navbar-toggle").addEventListener(click, function(){
-	document.getElementById("menu-container").classList.toggle.("menu-ver");
-});
-/*--------------------------------*/
