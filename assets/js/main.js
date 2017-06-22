8aad465047acfee5383e25d5acc1d4182b5c5106
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-22 10:51:56 -0400

    añado js listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,67 @@
-function initMap(){
-  var map = new google.maps.Map(document.getElementById("map"),{
+unction initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
-    center: {lat: -33.4724712, lng: -70.9107133},
+    center: {lat: -9.1191427, lng: -77.0349046},
     mapTypeControl: false,
     zoomControl: false,
     streetViewControl: false
   });
 
-
   function buscar(){
-    if(navigator.geolocation){
+    if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
     }
   }
 
-  //document.getElementById("tarifa").addEventListener("load",buscar);
+  document.getElementById("encuentrame").addEventListener("click", buscar);
   var latitud, longitud;
-
-  var funcionExito = function(posicion){
+  var funcionExito = function(posicion) {
     latitud = posicion.coords.latitude;
     longitud = posicion.coords.longitude;
 
     var miUbicacion = new google.maps.Marker({
-      position : {lat: latitud, lng: longitud},
+      position: {lat:latitud, lng:longitud},
       animation: google.maps.Animation.DROP,
-      map: map,
+      map: map
     });
 
     map.setZoom(17);
-    map.setCenter({lat: latitud, lng: longitud});
+    map.setCenter({lat:latitud, lng:longitud});
   }
 
-  var funcionError = function(error){
-    alert("Tenemos problemas encontrando tu ubicación");
-  }  
 
-  buscar();
-   
+
+  var funcionError = function (error) {
+    alert("Tenemos un problema con encontrar tu ubicación");
+  }
+
+  var start = document.getElementById('origen');
+  var end = document.getElementById('destino');
+
+  new google.maps.places.Autocomplete(start);
+  new google.maps.places.Autocomplete(end);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calcularRuta = function(directionsService,directionsDisplay){
+    directionsService.route({
+    origin: start.value,
+    destination: end.value,
+    travelMode: 'DRIVING'
+    }, function (response,status){
+      if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        }else{
+      window.alert("No encontramos una ruta");
+      }
+    })
+  };
+
+    directionsDisplay.setMap(map);
+    var trazarRuta = function(){
+      calcularRuta(directionsService,directionsDisplay);
+    };
+
+    document.getElementById("ruta").addEventListener('click', trazarRuta);  
+};
